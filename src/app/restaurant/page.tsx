4b824df812adacbb0a84400c033106011ad48821
oishi <suginami2357@gmail.com>
2025-03-06 08:55:50 +0900

    ブラウザのフッターを考慮してheightを明示的に指定

--- src/app/restaurant/page.tsx
+++ src/app/restaurant/page.tsx
@@ -6,6 +6,7 @@ import Sidebar from "@/components/ui/sidebar";
 import { useDevice } from "@/hooks/useDevice";
 import { useFetchGenre } from "@/hooks/useFetchGenre";
 import { useFetchRestaurant } from "@/hooks/useFetchRestaurant";
+import { useScroll } from "@/hooks/useScroll";
 import type { SearchParams } from "@/types/restaurant";
 import clsx from "clsx";
 import type React from "react";
@@ -14,7 +15,7 @@ import CreditDisplay from "../../components/restaurants/CreditDisplay";
 
 export default function Page() {
 	const { isMobile } = useDevice();
-	// const { scrollY } = useScroll();
+	const { scrollY } = useScroll();
 
 	const [isModalOpen, setIsModalOpen] = useState(false);
 	const [params, setParams] = useState<SearchParams>({ genres: [] });
@@ -27,7 +28,7 @@ export default function Page() {
 	const { isLoading } = fetch;
 
 	return (
-		<div className="flex flex-col items-center bg-white">
+		<div className="flex flex-col items-center h-[100dvh] bg-white">
 			<CreditDisplay className="h-2 text-[6px] text-gray-600" />
 			<RestaurantList fetch={fetch} params={params} />
 
@@ -36,14 +37,14 @@ export default function Page() {
 					className={clsx(
 						"fixed flex items-center justify-center z-30 left-2 bottom-8 w-12 h-12 bg-white text-gray-900 rounded-full shadow-lg",
 						isMobile &&
-							// scrollY > 5000 &&
+							scrollY > 5000 &&
 							"bg-white/50 backdrop-blur-[6px] backdrop-contrast-[4]",
 					)}
-					// style={
-					// 	isMobile
-					// 		? { opacity: scrollY < 5000 ? 1 - scrollY / 10000 : 1 }
-					// 		: {}
-					// }
+					style={
+						isMobile
+							? { opacity: scrollY < 5000 ? 1 - scrollY / 10000 : 1 }
+							: {}
+					}
 					isOpen={isModalOpen}
 					setIsOpen={setIsModalOpen}
 				/>
