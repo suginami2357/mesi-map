0dce7bb3d8543429802e8f56f8a01f7cbe991054
oishi <suginami2357@gmail.com>
2025-03-12 07:54:31 +0900

    サイドバーを開いていてもスクロール出来るように修正

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -8,11 +8,13 @@ export default function Sidebar({ isOpen, children, ...rest }: SidebarProps) {
 	return (
 		<div
 			className={clsx(
-				"flex fixed inset-0 z-20 shadow-md transform transition-transform duration-300 ease-in-out",
+				"flex fixed inset-0 z-20 shadow-md transform transition-transform duration-300 ease-in-out pointer-events-none",
 				isOpen ? "translate-x-0" : "-translate-x-full",
 			)}
 		>
-			<div {...rest}>{children}</div>
+			<div {...rest} className={clsx(rest.className, "pointer-events-auto")}>
+				{children}
+			</div>
 		</div>
 	);
 }
